f5751c022367557f9fbdd82302f2614a2bd3d612
phduchuy <phanhoangduchuyy@gmail.com>
2025-04-06 13:49:29 +0700

    config: config base endpoint

---
+++ src/api/config.ts
@@ -0,0 +1,104 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_ENDPOINT || "",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+let isRefreshing = false;
+let refreshSubscribers: ((token: string) => void)[] = [];
+
+async function refreshToken() {
+  try {
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (!refreshToken) throw new Error("No refresh token available");
+
+    const refreshApi = axios.create({
+      baseURL: import.meta.env.VITE_BASE_ENDPOINT || "",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const response = await refreshApi.post("/v1/auth/refresh-token", {
+      refresh_token: refreshToken,
+    });
+
+    console.log(response);
+    const newAccessToken = response.data.data.access_token;
+    console.log(response.data.data.access_token);
+
+    localStorage.setItem("accessToken", newAccessToken);
+
+    return newAccessToken;
+  } catch (error) {
+    console.error("Failed to refresh token", error);
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    window.location.href = "/login";
+    return null;
+  }
+}
+
+function onRefreshed(token: string) {
+  refreshSubscribers.forEach((callback) => callback(token));
+  refreshSubscribers = [];
+}
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401) {
+      console.error("Unauthorized! Redirecting to login...");
+      localStorage.removeItem("accessToken");
+      window.location.href = "/login";
+      return Promise.reject(error);
+    }
+
+    if (
+      error.response.status === 403 &&
+      error.response.data?.error?.code === "ERR.TOK0105" &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          refreshSubscribers.push((token) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            resolve(api(originalRequest));
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      const newToken = await refreshToken();
+      isRefreshing = false;
+
+      if (newToken) {
+        onRefreshed(newToken);
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return api(originalRequest);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
